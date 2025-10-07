c3afa5ce10e0406d818b83c147dac03426398406
Zoe Jimenez <zxejimenez@gmail.com>
2025-10-07 01:07:24 -0300

    ejercicios clase 7

--- clase7/clase7.cpp
+++ clase7/clase7.cpp
@@ -22,7 +22,7 @@ int main()
 {
     //   1. Ingresar un valor N (< 25). Generar un arreglo de N componentes en el cual las mismas
     // contengan los primeros números naturales pares e imprimirlo.
-    int input1;
+    int input1, input2, input3;
     cout << "Ingrese un numero menor a 25" << endl;
     cin >> input1;
     if (cin.fail() || input1 <= 0 || input1 >= 25)
@@ -36,5 +36,32 @@ int main()
         cout << num << " ";
     }
     cout << endl;
+
+    //     2. Ingresar un valor entero N (< 30) y a continuación un conjunto de N elementos. Si el último
+    // elemento del conjunto tiene un valor menor que 10 imprimir los negativos y en caso contrario los
+    // demás.
+    vector<int> elementos;
+    cout << "Ingrese un numero menor a 30" << endl;
+    cin >> input2;
+    cout << "Ingrese " << input2 << " elementos" << endl;
+    for (int i = 0; i < input2; i++)
+    {
+        int elemento;
+        cin >> elemento;
+        elementos.push_back(elemento);
+    }
+    if (elementos[input2 - 1] < 10)
+    {
+        for (int num : elementos)
+        {
+            if (num < 0)
+                cout << num << " ";
+        }
+    }
+    // 3. Ingresar un valor entero N (< 20). A continuación ingresar un conjunto VEC de N componentes.
+    // A partir de este conjunto generar otro FACT en el que cada elemento sea el factorial del elemento
+    // homólogo de VEC. Finalmente imprimir ambos vectores a razón de un valor de cada uno por
+    // renglón
+
     return 0;
 }
